9c63485731cee514f165f623c2e750076e2f1fd6
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2024-07-10 14:46:33 +0100

    okay

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -12,6 +12,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    return Column(
+      children: [
+        const Text('The question .....'),
+        const SizedBox(height: 30),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+        /////////////
+      const Text('The question .....'),
+        const SizedBox(height: 30),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+        //////////////////////
+        const Text('The question .....'),
+        const SizedBox(height: 30),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+          
+      ],
+    );
   }
 }
