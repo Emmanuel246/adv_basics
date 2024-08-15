3ad264554c6cf4c94fa9a5baf1cf6bad319173d8
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2024-08-15 05:04:35 +0100

    improving

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/start_screen.dart';
 import 'package:adv_basics/question_screen.dart';
+import 'package:adv_basics/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -29,7 +30,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       selectedAnswers = [];
       setState(() {
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
 
@@ -41,6 +42,10 @@ class _QuizState extends State<Quiz> {
 
     if (activeScreen == 'questions-screen') {
       screenWidget =  QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }
+
+    if(activeScreen == 'results-screen') {
+      screenWidget =  ResultsScreen(chooseAnswer: selectedAnswers,);
     }
 
     return MaterialApp(
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,7 +1,25 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 
+
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({super.key, required this.chooseAnswer});
+
+  final List<String> chooseAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chooseAnswer.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i]
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,10 +27,17 @@ class ResultsScreen extends StatelessWidget {
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
-        child: const Column(
+        child:  Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('You answered X out of Y questions correctly'),
+            const Text('You answered X out of Y questions correctly'),
+             const SizedBox(height: 30,),
+             const Text('List of answers and questions.....'),
+             const SizedBox(height: 30,),
+            TextButton(onPressed: () {}, child: const Text('Restart Quiz'))
+
+
+            
           ],
           )
           ,
