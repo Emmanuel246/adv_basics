cf82392164710da133518917bca93b6be225e13f
Makanjuola Emmanuel <makanjuolaiseoluwa1@gmail.com>
2024-08-15 06:03:34 +0100

    filtering list

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -24,6 +24,11 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
     return SizedBox(
       width: double.infinity,
       child: Container(
